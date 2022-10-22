72a0dd828ff1cadadd81e4c61fba1681105a058a
Naito888 <nioscia@gmail.com>
2022-10-22 11:56:34 +0000

    Gemfile記載

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "50761572d5fa4a16bf16061ec11dc746.vfs.cloud9.us-east-1.amazonaws.com"
+  
 end
