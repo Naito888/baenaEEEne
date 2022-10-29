14b2af79721acc93442c5f6cffc99587082e5801
Naito888 <nioscia@gmail.com>
2022-10-29 07:21:37 +0000

    deviceのストロングパラメーター追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
-
-  protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def customer_shut_out
-    unless admin_signed_in?
-      redirect_to top_path
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
