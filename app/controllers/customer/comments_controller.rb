b15b08ef3f120da232cb1c02bcffbd671ce3ecde
Naito888 <nioscia@gmail.com>
2022-11-03 12:22:50 +0000

    コメント機能の実装

--- app/controllers/customer/comments_controller.rb
+++ app/controllers/customer/comments_controller.rb
@@ -1,29 +1,29 @@
 class Customer::CommentsController < ApplicationController
-  
+
   def index
   end
-  
+
   def edit
   end
-  
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.new(comment_params)
-    @comment.post_id = @post
-    @comment.save
+    comment = current_customer.comments.new(comment_params)
+    comment.post_id = @post.id
+    comment.save
     redirect_to customer_post_path(@post)
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
   def comment_params
-    params.permit(:comment)
+    params.require(:comment).permit(:comment)
   end
-  
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :profile_image
 
   def get_profile_image(width, height)
