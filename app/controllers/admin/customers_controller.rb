6c8627c5a1a2364e71807be2c720f38cdde79dca
Naito888 <nioscia@gmail.com>
2022-11-10 08:58:42 +0000

    管理者側ユーザ画面の作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,18 +1,24 @@
 class Admin::CustomersController < ApplicationController
+  
   def index
+    @customers = Customer.page(params[:page])
   end
-  
+
   def show
+
   end
-  
+
   def edit
   end
-  
+
   def destroy
   end
-  
+
   def update
   end
-  
-  
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :profile_image, :is_deleted)
+  end
+
 end
