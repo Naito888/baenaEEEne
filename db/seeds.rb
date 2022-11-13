89a2bb4eaf5b8073e1d632f74e0bc61d2741cf63
Naito888 <nioscia@gmail.com>
2022-11-13 08:15:44 +0000

    seed追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(name: 'administrator', email: 'admin@gmail.com', password: 'admin_pass')
+
+
+customers = Customer.create!(
+  [
+    {email: 'mikan@gmail.com', name: 'みかんのかわ', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {email: 'rice@gmail.com', name: 'ごはん', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'chabashira@gmail.com', name: 'ちゃばしら', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+  ]
+)
+
+Post.create!(
+  [
+    {image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"), caption: 'ジャガイモがとれました', customer_id: customers[0].id },
+    {image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"), caption: 'ごはん派だけど朝はやっぱりトーストだよね', customer_id: customers[1].id },
+    {image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), caption: '卵ってわれやすいらしい', customer_id: customers[2].id }
+  ]
+)
