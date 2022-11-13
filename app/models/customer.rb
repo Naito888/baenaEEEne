f714b9b0fe818d36905d687fb9c1e7afc2197e21
Naito888 <nioscia@gmail.com>
2022-11-13 10:23:22 +0000

    レイアウト調整

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,8 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, {presence: true, length: {maximum: 8}}
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
